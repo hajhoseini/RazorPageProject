e7039c1cf4f16419e51f4271ab32208c3da439cf
a.hoseini <a.hoseini@DPK-15>
2024-11-02 13:30:25 +0330

    Create

---
+++ RazorPageProject/Pages/Admin/Product/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPageProject.Services;
+
+namespace RazorPageProject.Pages.Admin.Product
+{
+    public class CreateModel : PageModel
+    {
+        [BindProperty]
+        public ProductDTO Product { get; set; }
+
+        private readonly IProductService _productService;
+
+        public CreateModel(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            var result = _productService.Add(Product);
+        }
+    }
+}
