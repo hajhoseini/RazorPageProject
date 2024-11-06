c07ce3a64e05bb8d58da693b21754a2606310516
a.hoseini <a.hoseini@DPK-15>
2024-11-06 14:09:52 +0330

    Handler

---
+++ RazorPageProject/Areas/Admin/Pages/Test/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageProject.Areas.Admin.Pages.Test
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+        }
+
+        public void OnPostSend()
+        {
+        }
+
+        public void OnPostRegister()
+        {
+        }
+
+        [NonHandler]
+        public void OnPosTest()
+        {
+        }
+    }
+}
